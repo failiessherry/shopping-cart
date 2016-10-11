aaa538934d7fa1ddc95784acb19a2fafb15045a1
failiessherry <714sagittarius@gmail.com>
2016-10-11 16:03:31 +0800

    Issue #5 Outputting Product Data

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -4,18 +4,20 @@
 @endsection
 @section('content')
     <div class="row">
+        @foreach( $Products  as $product)
         <div class="col-sm-6 col-md-4">
             <div class="thumbnail">
-                <img src="https://pbs.twimg.com/media/CokbYlAUkAAlzRh.jpg:large" alt="...">
+                <img src="{{$product->imagePath}}" alt="...">
                 <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, mauris elit libero, lacus volutpat, eu consectetuer justo eu pellentesque. Impedit vestibulum donec, vivamus mi, ut pellentesque porta justo enim eu, in porttitor massa enim inceptos non sed. Ultricies velit. In ut, rhoncus mi facilisis, tellus diam consectetuer mi vestibulum ipsum platea, nec quis elit et id sem vero.</p>
+                    <h3>{{$product->title}}</h3>
+                    <p class="description">{{$product->description}}</p>
                     <div class="clearfix">
-                      <div class="pull-left price">$ 100</div>
+                      <div class="pull-left price">$ {{$product->price}}</div>
                       <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
                     </div>
                 </div>
             </div>
         </div>
+    @endforeach
     </div>
 @endsection
